2e0d8b27a080885d55cbd6b4d1e24551ad8c6466
Omid Ashtari <omid@Omids-Mac-mini-2.local>
2019-11-17 20:18:50 -0800

    my sincere apologies, I was so deep in trying to make this make sense that I forgot to make any commits :( -- this is the mvp commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import "./index.css";
+import Numbers from "./components/ButtonComponents/NumberButtons/Numbers";
+import Operators from "./components/ButtonComponents/OperatorButtons/Operators";
+import Specials from "./components/ButtonComponents/SpecialButtons/Specials";
+
+
 // STEP 4 - import the button and display components
 // Don't forget to import any extra css/scss files you build into the correct component
 
@@ -12,11 +18,14 @@ function App() {
   // Your functions should accept a parameter of the the item data being displayed to the DOM (ie - should recieve 5 if the user clicks on
   // the "5" button, or the operator if they click one of those buttons) and then call your setter function to update state.
   // Don't forget to pass the functions (and any additional data needed) to the components as props
-
+  const [number, setNumber] = useState()
   return (
     <div className="container">
       <Logo />
       <div className="App">
+        <Numbers />
+        <Operators />
+        <Specials />
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
       </div>
     </div>
@@ -24,3 +33,11 @@ function App() {
 }
 
 export default App;
+
+      // <DataProps data={data} />
+      // <Greeting person="Brandi" />
+      // <PersonDescription favoriteNumber={favNumber} description="SL" />
+      // <Greeting person="Emily" />
+      // <PersonDescription favoriteNumber={favNumber} description="Teach" />
+      // <Greeting person="Cristina" />
+      // <PersonDescription favoriteNumber={favNumber} description="SL" />
